bfa1841e4e0db27d3e8e984c874841bc4c8fc612
Taylor Donlan <taylordonlan@gmail.com>
2018-03-07 18:17:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
 
+return true if word.match(/^[aAeEiIoOuU]+\w/)
+
+return false  #only if above is not true
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
 
+  text.scan(/\b\w{5}\b/)
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  return true if text.match(/^[A-Z].+[\.!?]$/)
 
+  return false
 end
 
 def valid_phone_number?(phone)
 
+  returnvalue = phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
 end
